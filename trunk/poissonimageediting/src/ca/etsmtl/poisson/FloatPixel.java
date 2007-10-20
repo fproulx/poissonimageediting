4add7511c9c5f3a300deb2405d0ad9a252a7c69b
francois.proulx <francois.proulx@6df91437-e03c-0410-b291-c5e6509ff455>
2007-10-20 07:59:13 +0000

    Added the new structure. Starting working on the actual algorithm.

---
+++ trunk/poissonimageediting/src/ca/etsmtl/poisson/FloatPixel.java
@@ -0,0 +1,8 @@
+package ca.etsmtl.poisson;
+
+public class FloatPixel extends Pixel<Float> {
+
+	public FloatPixel(Float r, Float g, Float b) {
+		super(r, g, b);
+	}
+}
---
+++ trunk/poissonimageediting/src/ca/etsmtl/poisson/Vector2f.java
@@ -0,0 +1,10 @@
+package ca.etsmtl.poisson;
+
+public class Vector2f {
+	final public float u, v;
+	
+	public Vector2f(float u, float v) {
+		this.u = u;
+		this.v = v;
+	}
+}
