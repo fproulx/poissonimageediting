92df6e4345654dc86cdc6e4743b283371fde9e15
olivier.bilodeau.1@gmail.com <olivier.bilodeau.1@gmail.com@6df91437-e03c-0410-b291-c5e6509ff455>
2007-10-22 15:21:23 +0000

    freed some space and re-import attempt

---
+++ branches/olivier-import/PoissonImageEditing/src/model/basetype/FloatPixel.java
@@ -0,0 +1,8 @@
+package model.basetype;
+
+public class FloatPixel extends Pixel<Float> {
+
+	public FloatPixel(Float r, Float g, Float b) {
+		super(r, g, b);
+	}
+}
---
+++ branches/olivier-import/PoissonImageEditing/src/model/basetype/Pixel.java
@@ -0,0 +1,11 @@
+package model.basetype;
+
+public abstract class Pixel<T> {
+	final public T r, g, b;
+	
+	public Pixel(T r, T g, T b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+}
