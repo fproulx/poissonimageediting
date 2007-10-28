6875909bea7eab324e1b113890354aa188e798de
jphilippeplante <jphilippeplante@6df91437-e03c-0410-b291-c5e6509ff455>
2007-10-28 05:13:16 +0000

    Interface utilisateur
    ouvrir, fermer image venant de l'image browser

---
+++ UI/src/ui/ImageBrowserScroll.java
@@ -0,0 +1,27 @@
+/*
+ * ImageBrowserScroll.java
+ * 
+ * Created on 2007-10-21, 11:37:00
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ui;
+
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+/**
+ *
+ * @author ag95300
+ */
+public class ImageBrowserScroll implements MouseWheelListener {
+    
+    public ImageBrowserScroll() {
+      
+    }
+    
+    public void mouseWheelMoved(MouseWheelEvent e) {
+    }
+}
