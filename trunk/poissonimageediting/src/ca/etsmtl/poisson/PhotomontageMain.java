e21779391f13dbb03451ef6b49f2d666bdd812b5
francois.proulx <francois.proulx@6df91437-e03c-0410-b291-c5e6509ff455>
2007-11-14 04:28:05 +0000

    youppi le codepath est pas mal fait.

--- trunk/poissonimageediting/src/ca/etsmtl/poisson/PhotomontageMain.java
+++ trunk/poissonimageediting/src/ca/etsmtl/poisson/PhotomontageMain.java
@@ -1,22 +1,40 @@
 package ca.etsmtl.poisson;
 
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
 /**
  * 
  * @author fproulx
  *
  */
-public class PhotomontageMain {
+public class PhotomontageMain extends JFrame {
 
+	BufferedImage srcImage;
+	BufferedImage maskImage;
+	BufferedImage destImage;
+	BufferedImage output;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		PhotomontageMain f = new PhotomontageMain();
+		f.compute();
+	}
+	
+	public PhotomontageMain() {
+		setSize(1024, 768);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public void compute() {
 		// top-level path
 		String testImgPath = "resources/images/tests/";
 
@@ -27,20 +45,28 @@ public class PhotomontageMain {
 		
 		try {
 			// Load all the images
-			BufferedImage srcImage = ImageIO.read(new File(testImgPath + srcSmallImagePath));
-			BufferedImage maskImage = ImageIO.read(new File(testImgPath + maskValidImagePath));
-			BufferedImage destImage = ImageIO.read(new File(testImgPath + dstImagePath));
+			srcImage = ImageIO.read(new File(testImgPath + srcSmallImagePath));
+			maskImage = ImageIO.read(new File(testImgPath + maskValidImagePath));
+			destImage = ImageIO.read(new File(testImgPath + dstImagePath));
 
 			// Setup the Poisson solver
 			PoissonPhotomontage photomontage = new PoissonPhotomontage(srcImage, maskImage, destImage, new Point(10, 10));
 			// Do the heavy lifting
-			BufferedImage output = photomontage.createPhotomontage();
+			output = photomontage.createPhotomontage();
+			repaint();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		
-		
+	}
+	
+	public void paint(Graphics g) {
+		if(output != null) {
+			g.drawImage(srcImage, 0, 0, null);
+			g.drawImage(maskImage, 0, srcImage.getHeight(), null);
+			g.drawImage(destImage, 0, destImage.getHeight() + srcImage.getHeight(), null);
+			g.drawImage(output, destImage.getWidth(), 0, null);
+		}
 	}
 
 }
