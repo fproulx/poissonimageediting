d36c22512a5ee78bcbfafd9d92a8343377c36871
francois.proulx <francois.proulx@6df91437-e03c-0410-b291-c5e6509ff455>
2007-11-14 03:12:00 +0000

    Plus de code pour PoissonPhotomontage.java On y est presque !

--- trunk/poissonimageediting/src/ca/etsmtl/poisson/exceptions/ComputationException.java
+++ trunk/poissonimageediting/src/ca/etsmtl/poisson/exceptions/ComputationException.java
@@ -1,5 +1,12 @@
 package ca.etsmtl.poisson.exceptions;
 
 public class ComputationException extends Exception {
+	private static final long serialVersionUID = -1982069004778632902L;
 
+	public ComputationException() {
+	}
+	
+	public ComputationException(String msg) {
+		super("The computation could not complete successfully because of: " + msg);
+	}
 }
