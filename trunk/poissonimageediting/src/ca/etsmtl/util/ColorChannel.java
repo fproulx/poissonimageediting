8b1f2834a11537773371c01b57975cfd0d3f20a9
francois.proulx <francois.proulx@6df91437-e03c-0410-b291-c5e6509ff455>
2007-11-17 22:36:23 +0000

    Even more refactoring

--- trunk/poissonimageediting/src/ca/etsmtl/util/ColorChannel.java
+++ trunk/poissonimageediting/src/ca/etsmtl/util/ColorChannel.java
@@ -1,13 +1,13 @@
 package ca.etsmtl.util;
 
-public enum ColorChannels {
+public enum ColorChannel {
 	 RED (0x00FF0000, 16),
 	 GREEN (0x0000FF00, 8),
 	 BLUE (0x000000FF, 0),
 	 ALPHA (0xFF000000, 24);
 	 
 	 private final int mask, shift;
-	 ColorChannels(int mask, int shift) {
+	 ColorChannel(int mask, int shift) {
 		 this.mask = mask;
 		 this.shift = shift;
 	 }
