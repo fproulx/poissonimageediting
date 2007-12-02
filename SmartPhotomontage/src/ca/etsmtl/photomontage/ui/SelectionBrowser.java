48cd0a1a97ccf5886529d5da0c22b32bd038f1ba
jphilippeplante <jphilippeplante@6df91437-e03c-0410-b291-c5e6509ff455>
2007-12-02 22:12:19 +0000

    Merged UI in SmartPhotomontage Eclipse project

---
+++ SmartPhotomontage/src/ca/etsmtl/photomontage/ui/SelectionBrowser.java
@@ -0,0 +1,75 @@
+package ca.etsmtl.photomontage.ui;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+
+import ca.etsmtl.photomontage.ui.containers.ImageHolder;
+import ca.etsmtl.photomontage.ui.containers.SelectionHolder;
+import ca.etsmtl.photomontage.ui.events.ImageFrameMouseListener;
+import ca.etsmtl.photomontage.ui.events.ImageFrameMouseMotionListener;
+import ca.etsmtl.photomontage.ui.events.SelectionBrowserMouseListener;
+
+import com.developpez.gfx.swing.drag.GhostGlassPane;
+import com.developpez.gfx.swing.drag.GhostMotionAdapter;
+
+
+/**
+ * 
+ * Selection Browser based on ImageBrowser
+ * 
+ * @author Olivier Bilodeau <olivier.bilodeau.1@gmail.com>, Kim Lebel
+ *         <lebel.kim@gmail.com>, Jean-Philippe Plante
+ *         <jphilippeplante@gmail.com>, Francois Proulx
+ *         <francois.proulx@gmail.com>
+ */
+public class SelectionBrowser extends JComponent {
+	private static final long serialVersionUID = 8651024289475405132L;
+
+	Dimension newSize = new Dimension();
+
+	public static List<ImageHolder> images = new ArrayList<ImageHolder>();
+
+	public final static int currentSize = 180;
+	
+	/** Creates a new instance of ImageBrowser */
+	public SelectionBrowser() {
+
+		initComponents();
+	}
+	
+	private void initComponents() {
+		setOpaque(true);
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		setPreferredSize(new Dimension(currentSize, 0));
+	}
+
+	/**
+	 * Ajoute une image dans le image browser
+	 * 
+	 * @param image est l'image en bufferedimage à ajouter
+	 * @param filename est le path de l'image
+	 */
+	public void addImage(SelectionHolder selHold) {
+
+		JLabel label = new JLabel();
+		label.setIcon(new ImageIcon(selHold.getScaledSourceImage()));
+
+		GhostGlassPane glassPane = (GhostGlassPane) UIApp.getApplication().getMainFrame().getGlassPane();
+		
+		SelectionBrowserMouseListener mouseListener = new SelectionBrowserMouseListener(glassPane, selHold.getSourceImage());
+		label.addMouseListener(mouseListener);
+		
+		GhostMotionAdapter mouseMotionListener = new GhostMotionAdapter(glassPane);
+		label.addMouseMotionListener(mouseMotionListener);
+		
+		add(label);
+		revalidate();
+	}
+}
---
+++ SmartPhotomontage/src/ca/etsmtl/photomontage/ui/events/SelectionBrowserMouseListener.java
@@ -0,0 +1,94 @@
+package ca.etsmtl.photomontage.ui.events;
+
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+
+import ca.etsmtl.photomontage.ui.ImageFrame;
+import ca.etsmtl.photomontage.ui.SelectionBrowser;
+import ca.etsmtl.photomontage.ui.UIApp;
+import ca.etsmtl.photomontage.ui.UIView;
+
+import com.developpez.gfx.swing.drag.GhostDropAdapter;
+import com.developpez.gfx.swing.drag.GhostGlassPane;
+
+//TODO: Fix bug disappearing lines ?!
+/**
+ *
+ * @author Olivier Bilodeau <olivier.bilodeau.1@gmail.com>, Kim Lebel
+ * <lebel.kim@gmail.com>, Jean-Philippe Plante
+ * <jphilippeplante@gmail.com>, Francois Proulx
+ * <francois.proulx@gmail.com>
+ * 
+ */
+
+public class SelectionBrowserMouseListener extends GhostDropAdapter {
+	
+	// Image that represents the selected part of the image
+	private BufferedImage srcImage;
+	
+	public SelectionBrowserMouseListener(GhostGlassPane glassPane, BufferedImage img) {
+		super(glassPane, null);
+
+		this.srcImage = img;
+	}
+	
+	public void mousePressed(MouseEvent e) {
+
+		Component c = e.getComponent();
+
+        glassPane.setVisible(true);
+
+        Point p = (Point) e.getPoint().clone();
+        SwingUtilities.convertPointToScreen(p, c);
+        SwingUtilities.convertPointFromScreen(p, glassPane);
+
+        glassPane.setPoint(p);
+        glassPane.setImage(srcImage);
+        glassPane.repaint();
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+		Component c = e.getComponent();
+
+		// Ghostly drag and drop
+        Point p = (Point) e.getPoint().clone();
+        SwingUtilities.convertPointToScreen(p, c);
+
+        Point eventPoint = (Point) p.clone();
+        SwingUtilities.convertPointFromScreen(p, glassPane);
+
+        glassPane.setPoint(p);
+        glassPane.setVisible(false);
+        glassPane.setImage(null);
+
+        // WARNING !!!! This is a big stinky HACK
+        
+        // Get ahold of the source and destination components for the drag-drop event
+        UIView appView = (UIView) UIApp.getApplication().getMainView();
+        JDesktopPane desktop = appView.getImageFramesDesktop();
+        SelectionBrowser selectionBrowser = appView.getSelectionBrowser();
+        
+        // Convert the point relative to the Desktop area
+        Point desktopConvertedPoint = SwingUtilities.convertPoint(selectionBrowser, e.getPoint(), desktop);
+        // find the component that under this point
+        Component component = SwingUtilities.getDeepestComponentAt(desktop, desktopConvertedPoint.x, desktopConvertedPoint.y);
+        //TODO: this should check if it is actually the right JLabel (inside an ImageFrame)
+        if(component instanceof JLabel) {
+        	JLabel dstComponent = (JLabel) component;
+        	ImageFrame frame = (ImageFrame) dstComponent.getParent().getParent().getParent().getParent().getParent();
+        	frame.getImageHolder();
+        	//TODO: Francois continue ici
+        }
+   
+        //TODO fix this!
+        //fireGhostDropEvent(new SelectionGhostDropEvent(srcImage, maskImage, eventPoint));
+	}
+}
